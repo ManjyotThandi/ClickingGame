5aaddd66ffb54e7f2deeb4385923d29970e2ab5a
Manjyot Thandi <manjyot.thandi@gmail.com>
2019-11-03 20:03:47 -0500

    Updates

---
+++ precache-manifest.e6293b47fbcf0e869b0be2d74d6ae2f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf9888b1eab062f3906bd742a28589ad",
+    "url": "/ClickingGame/index.html"
+  },
+  {
+    "revision": "491d81ab321987972f8a",
+    "url": "/ClickingGame/static/css/main.ff067949.chunk.css"
+  },
+  {
+    "revision": "d76bf7d8a48f2421668c",
+    "url": "/ClickingGame/static/js/2.aafec028.chunk.js"
+  },
+  {
+    "revision": "491d81ab321987972f8a",
+    "url": "/ClickingGame/static/js/main.0c8ff580.chunk.js"
+  },
+  {
+    "revision": "79688e811fd76e6cbc10",
+    "url": "/ClickingGame/static/js/runtime-main.ebf6619a.js"
+  }
+]);
